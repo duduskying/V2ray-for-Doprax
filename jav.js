d1be188c44d32d932235f6c77e3ec43e188dfa73
jav.js <48942328+duduskying@users.noreply.github.com>
2023-07-16 12:21:51 +0800

    Update jav.js

--- jav.js
+++ jav.js
@@ -9,8 +9,8 @@
 *******************************
 [rewrite_local]
 # > javdb 解锁永久会员
-^https?:\/\/.*\/api\/v1\/users url script-response-body https://github.com/duduskying/V2ray-for-Doprax/blob/main/jav.js
-^https?:\/\/.*\/api\/v1\/(movies\/.*\/play|startup) url script-request-header https://github.com/duduskying/V2ray-for-Doprax/blob/main/jav.js
+^https?:\/\/.*\/api\/v1\/users url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/javbdvipcrack.js
+^https?:\/\/.*\/api\/v1\/(movies\/.*\/play|startup) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/javbdvipcrack.js
 [mitm] 
 hostname = api.pxxgg.xyz, api.ujvnmkx.cn, jdforrepam.com, api.yijingluowangluo.xyz, api.wwwuh5.cn, api.liquidlink.cn
 *******************************/
